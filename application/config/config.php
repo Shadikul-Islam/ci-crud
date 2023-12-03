dccb6978c6dc3292b98fb74c1111e1f52c4b8ab2
Shadikul Islam <shadikul.islam@datasoft-bd.com>
2023-12-03 11:16:08 +0600

    Modified config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.26.145/ci-app/index.php';
+$config['base_url'] = 'http://192.168.26.145/ci-crud/index.php';
 
 /*
 |--------------------------------------------------------------------------
